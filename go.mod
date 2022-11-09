f5efb35ab3201587058f7f8121e1c96cd30d1649
Elfi_Y <29664811+elfiyang16@users.noreply.github.com>
2022-11-09 21:34:29 +0000

    add c code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/elfiyang16/cgocgocgo
+
+go 1.19
--- greeter.h
+++ greeter.h
@@ -1,4 +1,9 @@
 #ifndef _GREETER_H
 #define _GREETER_H
-int greet(const char *name, int year, char *out);
+struct Greetee {
+    const char *name;
+    int year;
+};
+
+int greet(struct Greetee *g, char *out);
 #endif
